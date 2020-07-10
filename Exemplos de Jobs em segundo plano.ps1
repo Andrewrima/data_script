7bf02cdf5e94e507348da7886d9a9170badbac70
andrewrido <55111036+andrewrido@users.noreply.github.com>
2020-07-09 22:34:19 -0300

    Add files via upload

---
+++ Exemplos de Jobs em segundo plano.ps1
@@ -0,0 +1,10 @@
+﻿#Cria um job e coloca em segundo plano
+Get-ChildItem d:\ -Recurse 
+Start-Job -Name GetAllFiles -ScriptBlock {Get-ChildItem d:\ -Recurse}
+
+#Verifica os Jobs existentes e abre-os 
+Get-Job -Name GetAllFiles
+Receive-Job -Name GetAllFiles
+
+#Verifica todos os Jobs e Elimina-os
+Get-Job | Stop-Job
