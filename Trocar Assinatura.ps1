3b5e715643c29c14299ef5c0b2eefbf8d619cca8
Andrewrima <52092406+Andrewrima@users.noreply.github.com>
2022-09-27 15:56:55 -0300

    Delete Trocar Assinatura.ps1

--- Trocar Assinatura.ps1
+++
@@ -1,5 +0,0 @@
-﻿$OutLookProfilePath = "HKCU:\Software\Microsoft\Office\16.0\Outlook\Profiles\" + $Profiles.Trim() + "\9375CFF0413111d3B88A00104B2A6676\00000002"
-# Update the registry settings where Outlook picks up its signature information
-If (Test-Path $TargetForSignatures) {
-   Get-Item -Path $OutlookProfilePath | New-Itemproperty -Name "New Signature" -value $SignatureName -Propertytype string -Force 
-   Get-Item -Path $OutlookProfilePath | New-Itemproperty -Name "Reply-Forward Signature" -value $SignatureName -Propertytype string -Force }
