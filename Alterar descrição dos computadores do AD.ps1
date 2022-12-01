caf4bb40837571356c8eb04f17280b5201d74452
Andrew Machado <andrew.machado@kapitalo.com.br>
2022-12-01 16:20:41 -0300

    01.12.22

---
+++ Alterar descrição dos computadores do AD.ps1
@@ -0,0 +1,9 @@
+﻿$hashtab = @{
+    "Computer1" = "User1"
+    "Computer2" = "User2"
+    "Computer3" = "User3"
+    }
+
+ForEach ($line in $hashtab.GetEnumerator()){    
+    Set-ADComputer -Identity $line.Name -Description $line.Value
+}
